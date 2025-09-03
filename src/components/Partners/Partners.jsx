99d079736a289f77b1a3cfbbdc7e64192d99ecc0
pixelperfectsolutions <194055200+Pixeldev007@users.noreply.github.com>
2025-09-03 11:03:17 +0530

    updated

--- src/components/Partners/Partners.jsx
+++ src/components/Partners/Partners.jsx
@@ -19,21 +19,36 @@ const Partners = () => {
     speed: 3000,
     autoplay: true,
     autoplaySpeed: 3000,
-    slidesToShow: 5,
+    slidesToShow: 1,
     slidesToScroll: 1,
     cssEase: 'linear',
     arrows: false,
+    centerMode: false,
+    variableWidth: true,
     responsive: [
       {
         breakpoint: 1024,
         settings: {
           slidesToShow: 3,
+          centerMode: false,
+          variableWidth: true
         }
       },
       {
-        breakpoint: 600,
+        breakpoint: 768,
         settings: {
-          slidesToShow: 2,
+          slidesToShow: 1,
+          centerMode: false,
+          variableWidth: true,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          centerMode: false,
+          variableWidth: true
         }
       }
     ]
